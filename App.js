cb15d50faaba52d6423424ee93b2ee0e0e3b3319
kevorklepedjian1 <kevorkbedroslepedjian@gmail.com>
2024-01-11 04:19:15 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
+import AppNavigation from './navigations/AppNavigation';
+
+const App = () => {
+  return (
+   
+      <AppNavigation />
+    
+  );
+};
+
+export default App;
---
+++ themes/index.js
@@ -0,0 +1,7 @@
+export const theme = { background: "#eab308",
+ text: "#eab308"}
+
+export const styles = {
+    background: {backgroundColor: theme.background},
+    text:{color: theme.text}
+};
